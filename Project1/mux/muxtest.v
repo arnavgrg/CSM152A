7d89ba28ff53aba2fe4f8b7a3cbc5a276e170452
Arnav Garg <arnavgrg@gmail.com>
2020-04-19 20:47:39 -0700

    Completed test bench for mux

--- Project1/mux/muxtest.v
+++ Project1/mux/muxtest.v
@@ -23,44 +23,138 @@ module muxtest;
 		sw0 = 0;
 		sw1 = 0;
 		select = 3'b000;
-		#100;
+		#10;
 		
 		//Not
 		$display("Test: Not; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		sw0 = 1;
-		#100;
+		#10;
 		$display("Test: Not; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 
 		//Buff
 		sw0 = 0;
 		select = 3'b001;
-		#100;
+		#10;
 		$display("Test: Buf; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		
 		sw0 = 1;
-		#100;
+		#10;
 		$display("Test: Buf; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
         
 		//Xnor
 		sw0 = 0;
 		sw1 = 0;
 		select = 3'b010;
-		#100;
+		#10;
 		$display("Test: XNOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		
 		sw1 = 1;
-		#100;
+		#10;
 		$display("Test: XNOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		
 		sw0 = 1;
-		#100;
+		#10;
 		$display("Test: XNOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		
 		sw1 = 0;
-		#100;
+		#10;
 		$display("Test: XNOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 
+		//Xor
+		sw0 = 0;
+		sw1 = 0;
+		select = 3'b011;
+		#10;
+		$display("Test: XOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		sw1 = 1;
+		#10;
+		$display("Test: XOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
 		
+		sw0 = 1;
+		#10;
+		$display("Test: XOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw1 = 0;
+		#10;
+		$display("Test: XOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		//Or
+		sw0 = 0;
+		sw1 = 0;
+		select = 3'b100;
+		#10;
+		$display("Test: OR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		sw1 = 1;
+		#10;
+		$display("Test: OR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw0 = 1;
+		#10;
+		$display("Test: OR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw1 = 0;
+		#10;
+		$display("Test: OR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		//Nor
+		sw0 = 0;
+		sw1 = 0;
+		select = 3'b101;
+		#10;
+		$display("Test: NOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		sw1 = 1;
+		#10;
+		$display("Test: NOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw0 = 1;
+		#10;
+		$display("Test: NOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw1 = 0;
+		#10;
+		$display("Test: NOR; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		//AND
+		sw0 = 0;
+		sw1 = 0;
+		select = 3'b110;
+		#10;
+		$display("Test: AND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		sw1 = 1;
+		#10;
+		$display("Test: AND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw0 = 1;
+		#10;
+		$display("Test: AND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw1 = 0;
+		#10;
+		$display("Test: AND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		//NAND
+		sw0 = 0;
+		sw1 = 0;
+		select = 3'b111;
+		#10;
+		$display("Test: NAND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
+		sw1 = 1;
+		#10;
+		$display("Test: NAND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw0 = 1;
+		#10;
+		$display("Test: NAND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+		
+		sw1 = 0;
+		#10;
+		$display("Test: NAND; sw0=%b, sw1=%b, sel=%b, out=%b",sw0,sw1,select, out);
+
 	end
       
 endmodule
